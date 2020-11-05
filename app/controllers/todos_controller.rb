42fdd895c59e94654bca52a21b9b31a4c95eff4b
sydneygold <sydney.goldberg.com>
2020-11-05 15:34:42 -0700

    Added update resource

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,7 @@
 class TodosController < ApplicationController
+    
+    before_action :find_todo, only: [:update, :destroy]
+    
     def index
         @todos = Todo.all
 
@@ -6,20 +9,30 @@ class TodosController < ApplicationController
     end
 
     def create 
-        @todo = Todo.create(
-            title: params[:title],
-            content: params[:content],
-            urgent: params[:urgent],
-            done: params[:done]
-        )
+        @todo = Todo.create(todo_params)
         render json: @todo, status: :created
     end
 
-    def destroy
-        @todo = Todo.find(params[:id])
+    def update
+        @todo.update(todo_params)
 
+        render json: @todo
+    end
+
+    def destroy
         @todo.destroy 
 
         render status: :no_content
     end
+
+    private
+
+    def find_todo
+        @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+        params.require(:todo).permit(:title, :content, :urgent, :done)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :todos, only: [:index, :create, :destroy]
+  resources :todos, only: [:index, :create, :destroy, :update]
 end
